8ec72c238aba20678c388665ff672fcf37cf60b3
Francesco <francsco8bassino@gmail.com>
2021-04-24 17:46:03 -0500

    making a basic rust proyect with cargo

---
+++ kvstore/target/debug/deps/kvstore-df24bb6aca18f996.d
@@ -0,0 +1,5 @@
+c:\Users\Francesco\Documents\Proyectos\Key-value-database\kvstore\target\debug\deps\kvstore-df24bb6aca18f996.rmeta: src\main.rs
+
+c:\Users\Francesco\Documents\Proyectos\Key-value-database\kvstore\target\debug\deps\kvstore-df24bb6aca18f996.d: src\main.rs
+
+src\main.rs:
---
+++ kvstore/target/debug/deps/kvstore-e78635debe6a2691.d
@@ -0,0 +1,5 @@
+c:\Users\Francesco\Documents\Proyectos\Key-value-database\kvstore\target\debug\deps\kvstore-e78635debe6a2691.rmeta: src\main.rs
+
+c:\Users\Francesco\Documents\Proyectos\Key-value-database\kvstore\target\debug\deps\kvstore-e78635debe6a2691.d: src\main.rs
+
+src\main.rs:
